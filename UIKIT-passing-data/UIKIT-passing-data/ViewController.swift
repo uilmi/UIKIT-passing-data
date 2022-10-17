c27e0f5234db1bfd0306bed9e415d990e43b09e4
Ulul Ilmi <uilmi@na.edu>
2022-10-17 09:34:39 +0700

    adding files

---
+++ UIKIT-passing-data/UIKIT-passing-data/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKIT-passing-data
+//
+//  Created by Ulul I. on 17/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
